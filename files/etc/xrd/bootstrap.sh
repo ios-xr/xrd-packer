b362d4b1b0a720838852e97f8b50e4731df7bc33
Ed Beaty <ebeaty@cisco.com>
2024-12-11 07:04:45 -0800

    .

--- files/etc/xrd/bootstrap.sh
+++ files/etc/xrd/bootstrap.sh
@@ -53,4 +53,4 @@ systemctl enable hugetlb-gigantic-pages
 
 # Add boot cmdline args
 non_isolated_cores=$(/etc/xrd/get_non_isolated_cores.sh $ISOLATED_CORES)
-grubby --update-kernel ALL --args "nohz_full=${ISOLATED_CORES} nohz=on skew_tick=1 intel_pstate=disable tsc=reliable nosoftlockup isolcpus=${ISOLATED_CORES} irqaffinity=${non_isolated_cores} hugepages=${HUGEPAGES_GB} rcu_nocbs=${ISOLATED_CORES} hugepagesz=1G default_hugepagesz=1G rcupdate.rcu_normal_after_boot=1"
+grubby --update-kernel ALL --args "nohz_full=${ISOLATED_CORES} nohz=on skew_tick=1 intel_pstate=disable tsc=reliable nosoftlockup isolcpus=${ISOLATED_CORES} irqaffinity=${non_isolated_cores} hugepages=${boot_hugepages} rcu_nocbs=${ISOLATED_CORES} hugepagesz=1G default_hugepagesz=1G rcupdate.rcu_normal_after_boot=1"
